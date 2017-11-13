82acab1f95a9aee1249a889f09b221ce9dfb706f
Marcelo Dias <marc29dias@yahoo.com.br>
2017-11-13 15:01:55 -0200

    Add files via upload

---
+++ WebApiFileUpload/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApiFileUpload.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="uploadedFile"></param>
+        [HttpPost]
+        [Route("upload")]
+        public void PostFile(IFormFile uploadedFile)
+        {
+            //TODO: Save file
+        }
+    }
+}
---
+++ WebApiFileUpload/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.Extensions.PlatformAbstractions;
+using System.IO;
+
+namespace WebApiFileUpload
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Exemplo File Upload",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact { Name = "Marcelo Dias", Email = "marc29dias@yahoo.com.br", Url = "www.marcdias.com.br" }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var xmlPath = Path.Combine(basePath, "WebApiFileUpload.xml");
+                c.IncludeXmlComments(xmlPath);
+                c.OperationFilter<FileUploadOperation>();
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Configurações do Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            
+            app.UseMvc();
+
+            //ao rodar a api a tela do swagger abre automaticamente
+            app.Run(context => {
+                context.Response.Redirect("/swagger");
+                return Task.CompletedTask;
+            });
+        }
+    }
+}
